ccacc4bb2129a8d815421d0398bc2c461dfc915d
Zeuxis Lo <seekstudio@gmail.com>
2015-06-19 18:17:53 +0800

    add dist task in gulp file and related make command

--- Makefile
+++ Makefile
@@ -1,5 +1,35 @@
+.PHONY: dist build
+
 all:
 	@echo "make server"
 
-run:
+install:
+	@wget http://dl.nwjs.io/v0.12.2/nwjs-v0.12.2-osx-x64.zip
+	@unzip nwjs-v0.12.2-osx-x64.zip
+	@mv nwjs-v0.12.2-osx-x64/nwjs.app .
+	@rm -rf nwjs-v0.12.2-osx-x64 nwjs-v0.12.2-osx-x64.zip
+
+deps: npm bower
+
+npm:
+	@npm install
+
+bower:
+	@node_modules/bower/bin/bower install
+
+dist:
+	@node_modules/.bin/gulp
+
+run: dist
 	@open nwjs.app
+
+bower-init:
+	@node_modules/bower/bin/bower init
+
+clean:
+	@rm -rf build
+	@rm -rf dist
+
+clean-all: clean
+	@rm -rf bower_components
+	@rm -rf node_modules
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp     = require('gulp'),
     del      = require('del');
+    sequence = require('gulp-sequence'),
+    usemin   = require('gulp-usemin'),
+    cssmin   = require('gulp-cssmin'),
+    uglify   = require('gulp-uglify'),
+    rev      = require('gulp-rev'),
+    replace  = require('gulp-replace');
 
-gulp.task('clean', function() {
+gulp.task('clean', function(cb) {
     del(['dist'], cb);
 });
 
-gulp.task('files', function () {
-    return gulp.src([
-        'src/assets/img/**/*',
-    ])
-    .pipe(gulp.dest(
-        'dist/assets/img'
-    ));
+gulp.task('images', function () {
+    return gulp
+            .src('src/assets/img/**/*')
+            .pipe(gulp.dest('dist/assets/img'));
+});
+
+gulp.task('views', function () {
+    return gulp
+            .src('src/views/*')
+            .pipe(gulp.dest('dist/views'));
 });
 
 gulp.task('render', function() {
@@ -26,4 +35,4 @@ gulp.task('render', function() {
             .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('default', sequence('clean', ['render', 'files']));
+gulp.task('default', sequence('clean', ['render', 'images', 'views']));
